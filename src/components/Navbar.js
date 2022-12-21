e38c598ac8e0535c4f863a9de098be66283ebbe8
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-21 17:48:40 +0300

    Create navbar and make it responsive for mobile, table and desktop devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,72 @@
+import { useState } from "react";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
+
 const Navbar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const expand = () => {
+    setIsExpanded((isExpanded) => !isExpanded);
+  };
+
   return (
     <main className=" w-full">
       <section className="container mx-auto py-5  flex items-center justify-between">
-        <article>
-          <h1>Ecycle</h1>
+        <article className="flex items-center">
+          <h1 className="text-orange font-bold text-3xl cursor-pointer">
+            <span className="text-text">Ecy</span>cle
+          </h1>
         </article>
-
-        <article>right</article>
+        <article className="hidden md:block">
+          <ul className="flex items-center md:gap-4 gap-10 text-text font-bold text-md cursor-pointer">
+            <li className="hover:text-textHeavy">For Home</li>
+            <li className="hover:text-textHeavy">For Business</li>
+            <li className="hover:text-textHeavy">Drop Off Location</li>
+            <li className="hover:text-textHeavy">View Schedule</li>
+          </ul>
+        </article>
+        <article className="hidden md:block">
+          <div className="flex items-center text-text font-bold text-md">
+            Login <MdArrowDropDown size={"1.5rem"} />
+          </div>
+        </article>
+        <article className="md:hidden">
+          <div
+            onClick={expand}
+            className="space-y-1 p-1.5 bg-white md:hidden cursor-pointer z-50"
+          >
+            <div className="w-6 h-0.5 bg-black"></div>
+            <div className="w-6 h-0.5 bg-black"></div>
+            <div className="w-6 h-0.5 bg-black"></div>
+          </div>
+          {isExpanded && (
+            <ul className=" absolute top-0 left-0 bg-bgHeavy text-white text-center rounded-b-3xl w-full space-y-10 p-10 z-20">
+              <li
+                className="cursor-pointer hover:text-textLight"
+                onClick={expand}
+              >
+                <a href={`#home`}>For Home</a>
+              </li>
+              <li
+                className="cursor-pointer hover:text-textLight"
+                onClick={expand}
+              >
+                <a href={`#about`}>For Business</a>
+              </li>
+              <li
+                className="cursor-pointer hover:text-textLight"
+                onClick={expand}
+              >
+                <a href={`#skills`}>Drop off Location</a>
+              </li>
+              <li
+                className="cursor-pointer hover:text-textLight"
+                onClick={expand}
+              >
+                <a href={`#projects`}>View Schedule</a>
+              </li>
+            </ul>
+          )}
+        </article>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        bg: "#024731",
+        bgHeavy: "#023625",
+        bgLight: "#DAE4E1",
+        button: "#1C8200",
+        section: "#F8F8F2",
+      },
+      colors: {
+        text: "#024731",
+        textLight: "#DAE4E1",
+        textHeavy: "#023625",
+        orange: "#EBA900",
+      },
+      fontSize: {
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "32px",
+        xxl: "40px",
+      },
+      height: {},
+      width: {},
+    },
+
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
