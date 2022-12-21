e214b2e4459100048595daa1e9625361eed6c6e3
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-21 16:51:16 +0300

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <main className=" w-full">
+      <section className="container mx-auto py-5  flex items-center justify-between">
+        <article>
+          <h1>Ecycle</h1>
+        </article>
+
+        <article>right</article>
+      </section>
+    </main>
+  );
+};
+
+export default Navbar;
